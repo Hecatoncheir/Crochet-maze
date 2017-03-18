4e50002902606ba5627fa40f3e869705365cef4f
Vitaliy Vostrikov <rasart.pro@gmail.com>
2017-03-18 19:46:15 +0300

    Init

---
+++ lib/components/page_view/page_view.dart
@@ -0,0 +1,6 @@
+library page_view_component;
+
+import 'package:angular2/core.dart';
+
+@Component(selector: 'page-view', templateUrl: "page_view.html")
+class PageView {}
---
+++ web/main.dart
@@ -0,0 +1,7 @@
+import 'package:angular2/platform/browser.dart';
+import 'package:crochet_maze/components/page_view/page_view.dart';
+
+@AngularEntrypoint()
+void main() {
+  bootstrap(PageView);
+}
