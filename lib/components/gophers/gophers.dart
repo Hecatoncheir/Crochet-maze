69dbd46fc139511d0af6be31b1b38dffecd2b806
Vitaliy Vostrikov <rasart.pro@gmail.com>
2017-03-19 15:03:30 +0300

    Change all

--- lib/components/gophers/gophers.dart
+++
@@ -1,6 +0,0 @@
-library gophers_component;
-
-import 'package:angular2/core.dart';
-
-@Component(selector: 'gophers', templateUrl: "gophers.html")
-class GophersComponent {}
--- web/main.dart
+++
@@ -1,9 +0,0 @@
-library crochet_maze;
-
-import 'package:angular2/platform/browser.dart';
-import 'package:crochet_maze/components/page_view/page_view.dart';
-
-@AngularEntrypoint()
-void main() {
-  bootstrap(PageViewComponent);
-}
